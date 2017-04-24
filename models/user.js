49ad4f2b88b09cbef412d0535e116a8030393a49
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-24 16:23:08 +0000

    Create user model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const uniqueValidator = require("mongoose-unique-validator");
+
+let UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    }
+});
+
+UserSchema.plugin(uniqueValidator);
+
+UserSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.passwordHash);
+};
+
+UserSchema.virtual("password").set(function(value) {
+    this.passwordHash = bcrypt.hashSync(value, 8);
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
