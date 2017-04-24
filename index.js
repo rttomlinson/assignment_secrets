fd83bbe27c4faf5712eb631547038f864d6e178d
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-24 16:50:23 +0000

    Add changes to index.js

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 "use strict";
 
+const app = require('express')();
+
+// Require and mount the cookie and body parser middlewares
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+
+// Set up express-handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/test");
+app.use((req, res, next) => {
+    if (mongoose.connection.readyState) {
+        next();
+    }
+    else {
+        mongoose.connect("mongodb://localhost/test")
+            .then(() => next());
+    }
+});
+
+const User = require('./models/User');
+const {
+    createSignedSessionId,
+    loginMiddleware,
+    logginInOnly,
+    loggedOutOnly
+} = require('./services/Session');
